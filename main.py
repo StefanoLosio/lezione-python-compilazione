694e728fc58f71a7bd0d8c1f0074b35f588bf7ee
Stefano Losio <stefanolosio2008@gmail.com>
2025-11-17 19:13:57 +0100

    Call rispristinaNote before creating agenda

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from API import api_primaria
 
 agenda=[] # test: {'titolo':'ciao','contenuto':'ciao','id':5}
 
+rispristinaNote(agenda)
+
 api_agenda =api_primaria(agenda)
 api_agenda.crea_agenda()
 
